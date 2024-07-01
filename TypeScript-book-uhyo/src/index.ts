9348a7628e92bd546c934578ad2666c1b4673cee
nkzwsh00 <carpediem.imk@gmail.com>
2024-07-01 21:52:19 +0900

    feat: Update index.ts to accept user input and display it

--- TypeScript-book-uhyo/src/index.ts
+++ TypeScript-book-uhyo/src/index.ts
@@ -2,8 +2,19 @@
 
 // console.log(message);
 
-const greeting: string = 'Hello, ';
-const target: string = 'world!!!';
-const text: string = greeting + target;
-console.log(text);
+// const greeting: string = 'Hello, ';
+// const target: string = 'world!!!';
+// const text: string = greeting + target;
+// console.log(text);
 
+import { createInterface } from 'readline';
+
+const readline = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+readline.question('文字列を入力してください：', (line) => {
+  console.log(`${line}が入力されました。`);
+  readline.close();
+});
