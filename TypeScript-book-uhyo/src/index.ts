24a021cba399e8381525384e8f8a98e13403cd02
nkzwsh00 <carpediem.imk@gmail.com>
2024-07-01 23:54:15 +0900

    feat: Refactor index.ts to implement FizzBuzz logic and improve user input prompt

--- TypeScript-book-uhyo/src/index.ts
+++ TypeScript-book-uhyo/src/index.ts
@@ -1,26 +1,29 @@
-// const message: string = 'Hello, world!';
-
-// console.log(message);
-
-// const greeting: string = 'Hello, ';
-// const target: string = 'world!!!';
-// const text: string = greeting + target;
-// console.log(text);
-
-import { createInterface } from 'readline';
+import { createInterface } from "readline";
 
 const readline = createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-// readline.question('文字列を入力してください：', (line) => {
-//   console.log(`${line}が入力されました。`);
-//   readline.close();
-// });
-
-readline.question('数値を入力してください：', (line) => {
+readline.question("数値を入力してください：", (line) => {
   const num: number = Number(line);
-  console.log(num + 1000);
+  for (let i = 1; i <= num; i++) {
+    if (i > 1) {
+      process.stdout.write(" ");
+    }
+    if (i % 3 === 0 && i % 5 === 0) {
+      process.stdout.write("FizzBuzz");
+      continue;
+    }
+    if (i % 3 === 0) {
+      process.stdout.write("Fizz");
+      continue;
+    }
+    if (i % 5 === 0) {
+      process.stdout.write("Buzz");
+      continue;
+    }
+    process.stdout.write(String(i));
+  }
   readline.close();
 });
