27fca21799a8de9c10d90587e0e33daa0ecebe5f
nkzwsh00 <carpediem.imk@gmail.com>
2024-07-01 21:55:13 +0900

    feat: Update index.ts to prompt user for a number and display the result

--- TypeScript-book-uhyo/src/index.ts
+++ TypeScript-book-uhyo/src/index.ts
@@ -14,7 +14,13 @@ const readline = createInterface({
   output: process.stdout,
 });
 
-readline.question('文字列を入力してください：', (line) => {
-  console.log(`${line}が入力されました。`);
+// readline.question('文字列を入力してください：', (line) => {
+//   console.log(`${line}が入力されました。`);
+//   readline.close();
+// });
+
+readline.question('数値を入力してください：', (line) => {
+  const num: number = Number(line);
+  console.log(num + 1000);
   readline.close();
 });
