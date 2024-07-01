0676f0a17a700bb9b180bf93cda25b09463d6fa9
nkzwsh00 <carpediem.imk@gmail.com>
2024-07-02 00:46:38 +0900

    feat: Refactor index.ts to use spread operator for object merging

--- TypeScript-book-uhyo/src/index.ts
+++ TypeScript-book-uhyo/src/index.ts
@@ -1,29 +1,10 @@
-import { createInterface } from "readline";
+const obj1 = {
+    bar: 456,
+    baz: 789,
+};
+const obj2 = {
+    foo: 123,
+    ...obj1,
+};
 
-const readline = createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-readline.question("数値を入力してください：", (line) => {
-  const num: number = Number(line);
-  for (let i = 1; i <= num; i++) {
-    if (i > 1) {
-      process.stdout.write(" ");
-    }
-    if (i % 3 === 0 && i % 5 === 0) {
-      process.stdout.write("FizzBuzz");
-      continue;
-    }
-    if (i % 3 === 0) {
-      process.stdout.write("Fizz");
-      continue;
-    }
-    if (i % 5 === 0) {
-      process.stdout.write("Buzz");
-      continue;
-    }
-    process.stdout.write(String(i));
-  }
-  readline.close();
-});
+console.log(obj2); // { foo: 123, bar: 456, baz: 789 }
