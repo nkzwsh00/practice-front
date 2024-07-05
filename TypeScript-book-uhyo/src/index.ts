81563586e67a9321c4dfb4cdeeb2bcd9960f8d40
nkzwsh00 <carpediem.imk@gmail.com>
2024-07-05 21:01:47 +0900

    feat: Parse user data from CSV file in index.ts

--- TypeScript-book-uhyo/src/index.ts
+++ TypeScript-book-uhyo/src/index.ts
@@ -11,8 +11,16 @@ Mary Sue,14,1
 `
 
 //ここに追記
+const lines: string[] = data.split('\n');
+const users: User[] = lines.map((line) => {
+    const [name, ageString, premiumUserString] = line.split(',');
+    const age = parseInt(ageString, 10);
+    const premiumUser = premiumUserString === '1';
+    return { name, age, premiumUser };
+})
 
 for (const user of users) {
+    if (user.name === '') continue;
     if (user.premiumUser) {
         console.log(`${user.name} (${user.age})はプレミアム会員です`)
     } else {
