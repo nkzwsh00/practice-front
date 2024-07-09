dabf199986420b6faa187242408c3966254e7f9e
nkzwsh00 <carpediem.imk@gmail.com>
2024-07-09 21:39:41 +0900

    feat: Add isAdult() method and setAge() function to User class

--- TypeScript-book-uhyo/src/class.ts
+++ TypeScript-book-uhyo/src/class.ts
@@ -1,11 +1,17 @@
 class User {
     name: string = "";
     age: number = 0;
+
+    isAdult(): boolean {
+        return this.age >= 20;
+    }
+
+    setAge(newAge: number): void {
+        this.age = newAge;
+    }
 }
 
 const uhyo = new User();
-console.log(uhyo.name);
-console.log(uhyo.age);
-
-uhyo.age = 26;
-console.log(uhyo.age);
+console.log(uhyo.isAdult());
+uhyo.setAge(26);
+console.log(uhyo.isAdult());
