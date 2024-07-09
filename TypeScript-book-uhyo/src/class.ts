056ed3c227cc20e4c360990f18745d7a7552b402
nkzwsh00 <carpediem.imk@gmail.com>
2024-07-09 22:10:03 +0900

    Refactor User class constructor and add name property initialization

--- TypeScript-book-uhyo/src/class.ts
+++ TypeScript-book-uhyo/src/class.ts
@@ -1,18 +1,63 @@
 class User {
-    name: string = "";
-    age: number = 0;
+    name: string;
+    #age: number;
 
     constructor(name: string, age: number) {
         this.name = name;
-        this.age = age;
-        
+        this.#age = age;
     }
 
-    isAdult(): boolean {
-        return this.age >= 20;
+    public isAdult(): boolean {  
+        return this.#age >= 20;
     }
 }
 
 const uhyo = new User("uhyo", 26);
 console.log(uhyo.name);
 console.log(uhyo.isAdult());
+
+
+// class User {
+//     static adminName: string = "uhyo";
+//     static getAdminUser() {
+//         return new User(User.adminName, 26);
+//     }
+//     name: string;
+//     age: number;
+
+//     constructor(name: string, age: number) {
+//         this.name = name;
+//         this.age = age;
+//     }
+
+//     isAdult(): boolean {
+//         return this.age >= 20;
+//     }
+// }
+
+// console.log(User.adminName);
+// const admin = User.getAdminUser();
+// console.log(admin.age);
+// console.log(admin.isAdult());
+
+// const uhyo = new User("uhyo", 26);
+// console.log(uhyo.adminName);
+
+// class User {
+//     name: string = "";
+//     age: number = 0;
+
+//     constructor(name: string, age: number) {
+//         this.name = name;
+//         this.age = age;
+        
+//     }
+
+//     isAdult(): boolean {
+//         return this.age >= 20;
+//     }
+// }
+
+// const uhyo = new User("uhyo", 26);
+// console.log(uhyo.name);
+// console.log(uhyo.isAdult());
