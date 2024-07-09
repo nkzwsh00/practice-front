f0465997d0a5ca247e14b4d231109c75539af004
nkzwsh00 <carpediem.imk@gmail.com>
2024-07-09 21:41:45 +0900

    Refactor User class constructor and add name property initialization

--- TypeScript-book-uhyo/src/class.ts
+++ TypeScript-book-uhyo/src/class.ts
@@ -2,16 +2,17 @@ class User {
     name: string = "";
     age: number = 0;
 
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+        
+    }
+
     isAdult(): boolean {
         return this.age >= 20;
     }
-
-    setAge(newAge: number): void {
-        this.age = newAge;
-    }
 }
 
-const uhyo = new User();
-console.log(uhyo.isAdult());
-uhyo.setAge(26);
+const uhyo = new User("uhyo", 26);
+console.log(uhyo.name);
 console.log(uhyo.isAdult());
