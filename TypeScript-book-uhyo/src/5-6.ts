42454a6e1aeed7977032360cbc14ac9c21ccf4ab
nkzwsh00 <carpediem.imk@gmail.com>
2024-07-10 21:44:08 +0900

    feat: Remove commented out code and refactor User class to use a factory function

--- TypeScript-book-uhyo/src/5-6.ts
+++ TypeScript-book-uhyo/src/5-6.ts
@@ -18,20 +18,31 @@
 // console.log(getMessage(uhyo, 'Hello!'));
 
 
-class User {
-    name: string;
-    age: number;
+// class User {
+//     name: string;
+//     age: number;
 
-    constructor(name: string, age: number) {
-        this.name = name;
-        this.age = age;
-    }
+//     constructor(name: string, age: number) {
+//         this.name = name;
+//         this.age = age;
+//     }
 
-    public getMessage(user: User, message: string): string {
-        return `${user.name}(${user.age}) [${message}]`;
-    }
+//     public getMessage(user: User, message: string): string {
+//         return `${user.name}(${user.age}) [${message}]`;
+//     }
+// }
+
+// const uhyo = new User('uhyo', 26);
+// console.log(uhyo.getMessage(uhyo, 'Hello!'));
+
+// 5.6.3
+
+
+function createUser(name: string, age: number) {
+    return (message: string) => {
+        return `${name}(${age}): ${message}`;
+    };
 }
 
-const uhyo = new User('uhyo', 26);
-console.log(uhyo.getMessage(uhyo, 'Hello!'));
-
+const getMessage = createUser('uhyo', 26);
+console.log(getMessage('Hello!'));
