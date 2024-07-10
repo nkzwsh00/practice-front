4d9ff495f83833c193a49809afc276380ab22110
nkzwsh00 <carpediem.imk@gmail.com>
2024-07-10 21:34:10 +0900

    feat: Refactor User class to use constructor and add getMessage method

---
+++ TypeScript-book-uhyo/src/5-6.ts
@@ -0,0 +1,37 @@
+// type User = {
+//     name: string;
+//     age: number;
+// }
+
+// function createUser(name: string, age: number): User {
+//     if (name === '') {
+//         throw new Error('name is empty');
+//     }
+//     return { name, age };
+// }
+
+// function getMessage(user: User, message: string): string {
+//     return `${user.name}(${user.age}) [${message}]`;
+// }
+
+// const uhyo = createUser('uhyo', 26);
+// console.log(getMessage(uhyo, 'Hello!'));
+
+
+class User {
+    name: string;
+    age: number;
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public getMessage(user: User, message: string): string {
+        return `${user.name}(${user.age}) [${message}]`;
+    }
+}
+
+const uhyo = new User('uhyo', 26);
+console.log(uhyo.getMessage(uhyo, 'Hello!'));
+
