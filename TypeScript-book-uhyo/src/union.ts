335475e149188751297fc929abfd0937c85bc03a
nkzwsh00 <carpediem.imk@gmail.com>
2024-07-12 00:09:38 +0900

    feat: Add age property to Animal and Human types, and update tama and uhyo objects with age values

--- TypeScript-book-uhyo/src/union.ts
+++ TypeScript-book-uhyo/src/union.ts
@@ -1,11 +1,25 @@
 type Animal = {
     species: string;
+    age: string;
 };
 type Human = {
     name: string;
+    age: number;
 };
 
 type User = Animal | Human;
-const tama: User = { species: 'Felis silvestris catus' };
-const uhyo: User = { name: 'uhyo' };
+const tama: User = {
+    species: 'Felis silvestris catus',
+    age: '永遠の命',
+};
+const uhyo: User = {
+    name: 'uhyo',
+    age: 26,
+};
 
+function showAge(user: User) {
+    const age = user.age;
+    console.log(age);
+}
+showAge(tama);
+showAge(uhyo);
