da1326acde8dc79e65a0e41bcaecccf6329bec4a
nkzwsh00 <carpediem.imk@gmail.com>
2024-07-12 00:17:37 +0900

    feat: Update Animal and Human types to use number for age property

--- TypeScript-book-uhyo/src/union.ts
+++ TypeScript-book-uhyo/src/union.ts
@@ -1,25 +1,45 @@
+// type Animal = {
+//     species: string;
+//     age: string;
+// };
+// type Human = {
+//     name: string;
+//     age: number;
+// };
+
+// type User = Animal | Human;
+// const tama: User = {
+//     species: 'Felis silvestris catus',
+//     age: '永遠の命',
+// };
+// const uhyo: User = {
+//     name: 'uhyo',
+//     age: 26,
+// };
+
+// function showAge(user: User) {
+//     const age = user.age;
+//     console.log(age);
+// }
+// showAge(tama);
+// showAge(uhyo);
+
 type Animal = {
     species: string;
-    age: string;
-};
-type Human = {
-    name: string;
     age: number;
 };
 
-type User = Animal | Human;
-const tama: User = {
-    species: 'Felis silvestris catus',
-    age: '永遠の命',
-};
-const uhyo: User = {
-    name: 'uhyo',
-    age: 26,
+type Human =  Animal & {
+    name: string;
 };
 
-function showAge(user: User) {
-    const age = user.age;
-    console.log(age);
+const tama: Animal = {
+    species: 'Felis silvestris catus',
+    age: 3,
+};
+
+const uhyo: Human = {
+    species: 'homo sapiens sapiens',
+    age: 26,
+    name: 'uhyo',
 }
-showAge(tama);
-showAge(uhyo);
