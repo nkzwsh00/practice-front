d57f4db572cb8b3bdc155d6381df2ee0169b07a7
nkzwsh00 <carpediem.imk@gmail.com>
2024-07-11 23:54:47 +0900

    feat: Refactor code to use factory function for User creation and add union types for User definition

--- TypeScript-book-uhyo/src/5-6.ts
+++ TypeScript-book-uhyo/src/5-6.ts
@@ -1,48 +1,48 @@
-// type User = {
-//     name: string;
-//     age: number;
+// // type User = {
+// //     name: string;
+// //     age: number;
+// // }
+
+// // function createUser(name: string, age: number): User {
+// //     if (name === '') {
+// //         throw new Error('name is empty');
+// //     }
+// //     return { name, age };
+// // }
+
+// // function getMessage(user: User, message: string): string {
+// //     return `${user.name}(${user.age}) [${message}]`;
+// // }
+
+// // const uhyo = createUser('uhyo', 26);
+// // console.log(getMessage(uhyo, 'Hello!'));
+
+
+// // class User {
+// //     name: string;
+// //     age: number;
+
+// //     constructor(name: string, age: number) {
+// //         this.name = name;
+// //         this.age = age;
+// //     }
+
+// //     public getMessage(user: User, message: string): string {
+// //         return `${user.name}(${user.age}) [${message}]`;
+// //     }
+// // }
+
+// // const uhyo = new User('uhyo', 26);
+// // console.log(uhyo.getMessage(uhyo, 'Hello!'));
+
+// // 5.6.3
+
+
+// function createUser(name: string, age: number) {
+//     return (message: string) => {
+//         return `${name}(${age}): ${message}`;
+//     };
 // }
 
-// function createUser(name: string, age: number): User {
-//     if (name === '') {
-//         throw new Error('name is empty');
-//     }
-//     return { name, age };
-// }
-
-// function getMessage(user: User, message: string): string {
-//     return `${user.name}(${user.age}) [${message}]`;
-// }
-
-// const uhyo = createUser('uhyo', 26);
-// console.log(getMessage(uhyo, 'Hello!'));
-
-
-// class User {
-//     name: string;
-//     age: number;
-
-//     constructor(name: string, age: number) {
-//         this.name = name;
-//         this.age = age;
-//     }
-
-//     public getMessage(user: User, message: string): string {
-//         return `${user.name}(${user.age}) [${message}]`;
-//     }
-// }
-
-// const uhyo = new User('uhyo', 26);
-// console.log(uhyo.getMessage(uhyo, 'Hello!'));
-
-// 5.6.3
-
-
-function createUser(name: string, age: number) {
-    return (message: string) => {
-        return `${name}(${age}): ${message}`;
-    };
-}
-
-const getMessage = createUser('uhyo', 26);
-console.log(getMessage('Hello!'));
+// const getMessage = createUser('uhyo', 26);
+// console.log(getMessage('Hello!'));
---
+++ TypeScript-book-uhyo/src/union.ts
@@ -0,0 +1,11 @@
+type Animal = {
+    species: string;
+};
+type Human = {
+    name: string;
+};
+
+type User = Animal | Human;
+const tama: User = { species: 'Felis silvestris catus' };
+const uhyo: User = { name: 'uhyo' };
+
