6f73208fcfeda520fb3b46c764bcdf3667b13226
nkzwsh00 <carpediem.imk@gmail.com>
2024-07-13 22:51:44 +0900

    Refactor Option type tags to use lowercase letters

--- TypeScript-book-uhyo/src/6-8.ts
+++ TypeScript-book-uhyo/src/6-8.ts
@@ -1,9 +1,9 @@
 type Some<T> = {
-    tag: 'Some';
+    tag: 'some';
     value: T;
 };
 type None = {
-    tag: 'None';
+    tag: 'none';
 };
 type Option<T> = Some<T> | None;
 
@@ -13,12 +13,27 @@ function showNumberIfExists(obj: Option<number>): void {
     }
 }
 
-const four: Option<number> = { tag: 'Some', value: 4 };
-const notihing: Option<number> = { tag: 'None' };
+const four: Option<number> = { tag: 'some', value: 4 };
+const notihing: Option<number> = { tag: 'none' };
 
-showNumberIfExists(four);
-showNumberIfExists(notihing);
+// showNumberIfExists(four);
+// showNumberIfExists(notihing);
 
 function isSome<T>(obj: Option<T>): obj is Some<T> {
-    return obj.tag === 'Some';
+    return obj.tag === 'some';
+}
+
+function doubleOption(obj: Option<number>) {
+    return mapOption(obj, x => x * 2);
+}
+
+console.log(doubleOption(four));
+console.log(doubleOption(notihing));
+
+function mapOption<T, U>(obj: Option<T>, f: (value: T) => U): Option<U> {
+    if (isSome(obj)) {
+        return { tag: 'some', value: f(obj.value) };
+    } else {
+        return { tag: 'none' };
+    }
 }
