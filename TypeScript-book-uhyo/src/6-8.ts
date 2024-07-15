66a58e92948edaf47bbe134e24640e0ecc1914a0
nkzwsh00 <carpediem.imk@gmail.com>
2024-07-15 20:16:28 +0900

    feat: Remove commented out code and refactor User class to use a factory function

--- TypeScript-book-uhyo/src/6-8.ts
+++ TypeScript-book-uhyo/src/6-8.ts
@@ -1,39 +1,39 @@
-type Some<T> = {
-    tag: 'some';
-    value: T;
-};
-type None = {
-    tag: 'none';
-};
-type Option<T> = Some<T> | None;
+// type Some<T> = {
+//     tag: 'some';
+//     value: T;
+// };
+// type None = {
+//     tag: 'none';
+// };
+// type Option<T> = Some<T> | None;
 
-function showNumberIfExists(obj: Option<number>): void {
-    if (isSome(obj)) {
-        console.log(obj.value);
-    }
-}
+// function showNumberIfExists(obj: Option<number>): void {
+//     if (isSome(obj)) {
+//         console.log(obj.value);
+//     }
+// }
 
-const four: Option<number> = { tag: 'some', value: 4 };
-const notihing: Option<number> = { tag: 'none' };
+// const four: Option<number> = { tag: 'some', value: 4 };
+// const notihing: Option<number> = { tag: 'none' };
 
-// showNumberIfExists(four);
-// showNumberIfExists(notihing);
+// // showNumberIfExists(four);
+// // showNumberIfExists(notihing);
 
-function isSome<T>(obj: Option<T>): obj is Some<T> {
-    return obj.tag === 'some';
-}
+// function isSome<T>(obj: Option<T>): obj is Some<T> {
+//     return obj.tag === 'some';
+// }
 
-function doubleOption(obj: Option<number>) {
-    return mapOption(obj, x => x * 2);
-}
+// function doubleOption(obj: Option<number>) {
+//     return mapOption(obj, x => x * 2);
+// }
 
-console.log(doubleOption(four));
-console.log(doubleOption(notihing));
+// console.log(doubleOption(four));
+// console.log(doubleOption(notihing));
 
-function mapOption<T, U>(obj: Option<T>, f: (value: T) => U): Option<U> {
-    if (isSome(obj)) {
-        return { tag: 'some', value: f(obj.value) };
-    } else {
-        return { tag: 'none' };
-    }
-}
+// function mapOption<T, U>(obj: Option<T>, f: (value: T) => U): Option<U> {
+//     if (isSome(obj)) {
+//         return { tag: 'some', value: f(obj.value) };
+//     } else {
+//         return { tag: 'none' };
+//     }
+// }
---
+++ TypeScript-book-uhyo/src/index1.ts
@@ -0,0 +1,29 @@
+// type User1 = {
+//     name: string;
+//     age: number;
+//     premiumUser: boolean;
+// }
+
+// const data: string = `
+// uhyo,26,1
+// John Smith,17,0
+// Mary Sue,14,1
+// `
+
+// //ここに追記
+// const lines: string[] = data.split('\n');
+// const users1: User1[] = lines.map((line) => {
+//     const [name, ageString, premiumUserString] = line.split(',');
+//     const age = parseInt(ageString, 10);
+//     const premiumUser = premiumUserString === '1';
+//     return { name, age, premiumUser };
+// })
+
+// for (const user of users1) {
+//     if (user.name === '') continue;
+//     if (user.premiumUser) {
+//         console.log(`${user.name} (${user.age})はプレミアム会員です`)
+//     } else {
+//         console.log(`${user.name} (${user.age})はプレミアム会員ではありません`)
+//     }
+// }
