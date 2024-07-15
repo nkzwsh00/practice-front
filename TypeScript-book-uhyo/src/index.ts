af6bdedd37365e5c75c971d0a3bbcde40e0a8dfc
nkzwsh00 <carpediem.imk@gmail.com>
2024-07-15 20:22:38 +0900

    feat: Add count variable to display the number of occurrences of "uhyo" in the text file

--- TypeScript-book-uhyo/src/index.ts
+++ TypeScript-book-uhyo/src/index.ts
@@ -1,6 +1,10 @@
+// 7章　力試し
+
 import {readFileSync} from "fs";
 
 const text = readFileSync("uhyo.txt", "utf-8");
 console.log(text);
 
+const count:number = text.indexOf("uhyo");
 
+console.log(count);
