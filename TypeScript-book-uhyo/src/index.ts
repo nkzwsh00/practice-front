1baf027e4e9ec70c79b5c5913996c31bc090d614
nkzwsh00 <carpediem.imk@gmail.com>
2024-07-16 23:52:47 +0900

    Refactor code to remove commented out code and optimize file path manipulation

--- TypeScript-book-uhyo/src/index.ts
+++ TypeScript-book-uhyo/src/index.ts
@@ -1,30 +1,30 @@
 // 7章　力試し
 
-import {readFileSync} from "fs";
-import path from "path";
-import {fileURLToPath} from "url";
+// import {readFileSync} from "fs";
+// import path from "path";
+// import {fileURLToPath} from "url";
 
-const __currentPath = fileURLToPath(import.meta.url);
-console.log(__currentPath);
-const __currentDirectory = path.dirname(__currentPath);
-console.log(__currentDirectory);
-const __filePath = path.join(__currentDirectory, "../uhyo.txt");
-console.log(__filePath);
+// const __currentPath = fileURLToPath(import.meta.url);
+// console.log(__currentPath);
+// const __currentDirectory = path.dirname(__currentPath);
+// console.log(__currentDirectory);
+// const __filePath = path.join(__currentDirectory, "../uhyo.txt");
+// console.log(__filePath);
 
-const text = readFileSync(__filePath, "utf-8");
-console.log(text);
+// const text = readFileSync(__filePath, "utf-8");
+// console.log(text);
 
-const textLength:number = text.length;
-let count:number = 0;
-let currentIndex:number = 0;
+// const textLength:number = text.length;
+// let count:number = 0;
+// let currentIndex:number = 0;
 
-while (currentIndex < textLength) {
-    const index = text.indexOf("uhyo", currentIndex);
-    if (index === -1) {
-        break;
-    }
-    currentIndex = index + 1;
-    count++;
-}
+// while (currentIndex < textLength) {
+//     const index = text.indexOf("uhyo", currentIndex);
+//     if (index === -1) {
+//         break;
+//     }
+//     currentIndex = index + 1;
+//     count++;
+// }
 
-console.log(count);
+// console.log(count);
