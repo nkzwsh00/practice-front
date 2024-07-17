dfa3cbe7f27ea46910170eb94c2d4475c7032c22
nkzwsh00 <carpediem.imk@gmail.com>
2024-07-17 23:28:08 +0900

    Refactor code to use async/await syntax for Promise resolution

--- TypeScript-book-uhyo/src/async.ts
+++ TypeScript-book-uhyo/src/async.ts
@@ -10,12 +10,39 @@
 //     console.log(value);
 // });
 
+// const sleep = (duration: number) => {
+//     return new Promise<void>((resolve) => {
+//         setTimeout(resolve, duration);
+//     })
+// };
+
+// sleep(3000).then(() => {
+//     console.log('3秒経過しました');
+// });
+
 const sleep = (duration: number) => {
     return new Promise<void>((resolve) => {
         setTimeout(resolve, duration);
-    })
-};
+    });
+}
 
-sleep(3000).then(() => {
-    console.log('3秒経過しました');
+async function get3(): Promise<number> {
+    console.log('get3関数を実行します');
+    await sleep(1000);
+    console.log('1秒経過しました');
+    return 3;
+}
+
+async function main() {
+    console.log('main関数を実行します');
+    const num1 = await get3();
+    const num2 = await get3();
+    const num3 = await get3();
+    console.log(num1, num2, num3);
+    return num1 + num2 + num3;
+}
+
+main().then(result => {
+    console.log(`result: ${result}`);
 });
+
