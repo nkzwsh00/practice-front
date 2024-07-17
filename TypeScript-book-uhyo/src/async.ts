07c4a65985c96660e2a387f27d210fa2ff2e456a
nkzwsh00 <carpediem.imk@gmail.com>
2024-07-18 00:11:06 +0900

    Refactor code to count occurrences of "uhyo" in the text file

--- TypeScript-book-uhyo/src/async.ts
+++ TypeScript-book-uhyo/src/async.ts
@@ -46,14 +46,43 @@
 //     console.log(`result: ${result}`);
 // });
 
-async function main() {
-    const { readFile, writeFile } = await import('fs/promises');
+// async function main() {
+//     const { readFile, writeFile } = await import('fs/promises');
 
-    const fooContent = await readFile('foo.txt', 'utf-8');
-    await writeFile('bar.txt', fooContent + fooContent);
-    console.log('ファイルの書き込みが完了しました');
+//     const fooContent = await readFile('foo.txt', 'utf-8');
+//     await writeFile('bar.txt', fooContent + fooContent);
+//     console.log('ファイルの書き込みが完了しました');
+// }
+
+// main().then(() => {
+//     console.log('main関数が完了しました');
+// });
+
+
+// 8章　力試し
+
+// import {readFileSync} from "fs";
+import {readFile} from "fs/promises";
+import path from "path";
+import {fileURLToPath} from "url";
+
+const __currentPath = fileURLToPath(import.meta.url);
+const __currentDirectory = path.dirname(__currentPath);
+const __filePath = path.join(__currentDirectory, "../uhyo.txt");
+
+const text = await readFile(__filePath, "utf-8");
+
+const textLength:number = text.length;
+let count:number = 0;
+let currentIndex:number = 0;
+
+while (currentIndex < textLength) {
+    const index = text.indexOf("uhyo", currentIndex);
+    if (index === -1) {
+        break;
+    }
+    currentIndex = index + 1;
+    count++;
 }
 
-main().then(() => {
-    console.log('main関数が完了しました');
-});
+console.log(count);
