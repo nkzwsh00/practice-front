fa0386b1b9d26fe15f1a46770da59cbca2a58423
nkzwsh00 <carpediem.imk@gmail.com>
2024-07-20 21:16:39 +0900

    feat: Add lazy loading for images in Gallery component

--- react-describing-the-ui/src/App.js
+++ react-describing-the-ui/src/App.js
@@ -1,8 +1,19 @@
-export default function Profile() {
+function Profile() {
   return (
     <img
       src="https://i.imgur.com/MK3eW3Am.jpg"
       alt="Katherine Johnson"
     />
   )
+}
+
+export default function Gallery() {
+  return (
+    <section>
+      <h1>Amazing scientists</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
+  );
 }
