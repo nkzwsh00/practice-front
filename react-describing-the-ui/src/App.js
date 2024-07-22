6993a6b5e8255ba0c26102e04191cdb0703fb977
nkzwsh00 <carpediem.imk@gmail.com>
2024-07-22 22:12:07 +0900

    feat: Update App component to display profile image with avatar class

--- react-describing-the-ui/src/App.js
+++ react-describing-the-ui/src/App.js
@@ -1,6 +1,17 @@
-import Gallery from "./Gallery";
-export default function App() {
+function Avatar() {
   return (
-    <Gallery />
+    <img
+      className="avatar"
+      src="https://i.imgur.com/1bX5QH6.jpg"
+      alt="Lin Lanying"
+      width={100}
+      height={100}
+    />
+  );
+}
+
+export default function Profile() {
+  return (
+    <Avatar />
   );
 }
