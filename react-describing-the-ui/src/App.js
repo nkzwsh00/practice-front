ead66b7e545780b4faa9f6f2c47ec7c72ed610e3
nkzwsh00 <carpediem.imk@gmail.com>
2024-07-22 22:46:11 +0900

    feat: Update Card component to display profile information

--- react-describing-the-ui/src/App.js
+++ react-describing-the-ui/src/App.js
@@ -1,17 +1,59 @@
-function Avatar() {
+import { getImageUrl } from './utils.js';
+
+function Card({ title, imageId, profession, awards, discovery }) {
   return (
-    <img
-      className="avatar"
-      src="https://i.imgur.com/1bX5QH6.jpg"
-      alt="Lin Lanying"
-      width={100}
-      height={100}
-    />
+    <section className="profile">
+      <h2>{title}</h2>
+      <img
+        className="avatar"
+        src={getImageUrl(imageId)}
+        alt={title}
+        width={70}
+        height={70}
+      />
+      <ul>
+        <li>
+          <b>Profession: </b>
+          {profession}
+        </li>
+        <li>
+          <b>Awards: {awards.length} </b>
+          ({awards.join(', ')})
+        </li>
+        <li>
+          <b>Discovered: </b>
+          {discovery}
+        </li>
+      </ul>
+    </section>
   );
 }
 
-export default function Profile() {
+export default function Gallery() {
   return (
-    <Avatar />
+    <div>
+      <h1>Notable Scientists</h1>
+      <Card
+        title="Katherine Johnson"
+        imageId="MK3eW3A"
+        profession="mathematician"
+        awards={['Presidential Medal of Freedom', 'NASA Group Achievement Award']}
+        discovery="mathematical calculations for orbital mechanics"
+      />
+      <Card
+        title="Maria Skłodowska-Curie"
+        imageId="szV5sdG"
+        profession="physicist and chemist"
+        awards={['Nobel Prize in Physics', 'Nobel Prize in Chemistry', 'Davy Medal', 'Matteucci Medal']}
+        discovery="polonium (chemical element)"
+      />
+      <Card
+        title="Katsuko Saruhashi"
+        imageId="YfeOqp2"
+        profession="geochemist"
+        awards={['Miyake Prize for geochemistry', 'Tanaka Prize']}
+        discovery="a method for measuring carbon dioxide in seawater"
+      />
+    </div>
   );
 }
