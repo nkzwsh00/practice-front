609af1f41769ce2d76b5ce70b4a0ff3f32b3c1f9
nkzwsh00 <carpediem.imk@gmail.com>
2024-07-23 23:23:26 +0900

    feat: Update App component to display recipes

--- react-rendering/src/App.js
+++ react-rendering/src/App.js
@@ -1,24 +1,9 @@
-import { people } from './data.js';
-import { getImageUrl } from './utils.js';
+import { recipes } from './data.js';
 
-export default function List() {
-  const listItems = people.map(person =>
-    <li key={person.id}>
-      <img
-        src={getImageUrl(person)}
-        alt={person.name}
-      />
-      <p>
-        <b>{person.name}:</b>
-        {' ' + person.profession + ' '}
-        known for {person.accomplishment}
-      </p>
-    </li>
-  );
+export default function RecipeList() {
   return (
-    <article>
-      <h1>Scientists</h1>
-      <ul>{listItems}</ul>
-    </article>
+    <div>
+      <h1>Recipes</h1>
+    </div>
   );
 }
