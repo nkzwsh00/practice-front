e6e4a4abdc6258330658c6871f3b8b4add01ae0b
nkzwsh00 <carpediem.imk@gmail.com>
2024-08-05 19:43:46 +0900

    feat: 画像の読み込みが適切に表示されるよう修正

--- study-react/src/App.tsx
+++ study-react/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 type Image = {
+  id: number;
   place: string;
   src: string;
 };
@@ -24,7 +25,7 @@ const Gallery = () => {
       <h3>
         Image {index + 1} of {images.length}
       </h3>
-      {image && <img src={image.src} />}
+      {image && <img key={image.id} src={image.src} />}
       {image && <p>{image.place}</p>}
     </>
   );
@@ -32,30 +33,37 @@ const Gallery = () => {
 
 const images: Image[] = [
   {
+    id: 0,
     place: "Penang, Malaysia",
     src: "https://i.imgur.com/FJeJR8M.jpg",
   },
   {
+    id: 1,
     place: "Lisbon, Portugal",
     src: "https://i.imgur.com/dB2LRbj.jpg",
   },
   {
+    id: 2,
     place: "Bilbao, Spain",
     src: "https://i.imgur.com/z08o2TS.jpg",
   },
   {
+    id: 3,
     place: "Valparaíso, Chile",
     src: "https://i.imgur.com/Y3utgTi.jpg",
   },
   {
+    id: 4,
     place: "Schwyz, Switzerland",
     src: "https://i.imgur.com/JBbMpWY.jpg",
   },
   {
+    id: 5,
     place: "Prague, Czechia",
     src: "https://i.imgur.com/QwUKKmF.jpg",
   },
   {
+    id: 6,
     place: "Ljubljana, Slovenia",
     src: "https://i.imgur.com/3aIiwfm.jpg",
   },
