1800320882abd5962c4e2f4c7a0c1849ce986feb
nkzwsh00 <carpediem.imk@gmail.com>
2024-08-13 21:47:27 +0900

    feat: 回答。右クリックからの再生の同期は、ブラウザの都合?で確認できなかった

--- study-react/src/App.tsx
+++ study-react/src/App.tsx
@@ -2,19 +2,27 @@ import { useState, useRef } from "react";
 
 export default function VideoPlayer() {
   const [isPlaying, setIsPlaying] = useState(false);
+  const videoRef = useRef<HTMLVideoElement>(null);
 
   function handleClick() {
     const nextIsPlaying = !isPlaying;
     setIsPlaying(nextIsPlaying);
+    if (nextIsPlaying) {
+      videoRef.current?.play();
+    } else {
+      videoRef.current?.pause();
+    }
   }
 
   return (
     <>
       <button onClick={handleClick}>{isPlaying ? "Pause" : "Play"}</button>
-      <video width="250">
+      <video width="250" ref={videoRef}>
         <source
           src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
           type="video/mp4"
+          onPlay={() => setIsPlaying(true)}
+          onPause={() => setIsPlaying(false)}
         />
       </video>
     </>
