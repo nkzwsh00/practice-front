303ab13464770be241ef93d326adb519e9c7a485
nkzwsh00 <carpediem.imk@gmail.com>
2024-08-14 21:06:56 +0900

    feat: 機能実装

--- study-react/src/Counter.tsx
+++ study-react/src/Counter.tsx
@@ -8,8 +8,11 @@ export default function Counter() {
       setCount((c) => c + 1);
     }
 
-    setInterval(onTick, 1000);
-  }, []);
+    const intervalId = setInterval(onTick, 1000);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [count]);
 
   return <h1>{count}</h1>;
 }
