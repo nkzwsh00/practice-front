f3e33980efc6f5c0f2f5450c9915c364c2a766e0
nkzwsh00 <carpediem.imk@gmail.com>
2024-08-17 00:28:31 +0900

    chore: 問題転記

--- study-react/src/App.tsx
+++ study-react/src/App.tsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function Form() {
+  const [showForm, setShowForm] = useState(true);
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    if (!showForm) {
+      sendMessage(message);
+    }
+  }, [showForm, message]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setShowForm(false);
+  }
+
+  if (!showForm) {
+    return (
+      <>
+        <h1>Thanks for using our services!</h1>
+        <button
+          onClick={() => {
+            setMessage("");
+            setShowForm(true);
+          }}
+        >
+          Open chat
+        </button>
+      </>
+    );
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <form onSubmit={handleSubmit}>
+      <textarea
+        placeholder="Message"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <button type="submit" disabled={message === ""}>
+        Send
+      </button>
+    </form>
+  );
 }
 
-export default App
+function sendMessage(message) {
+  console.log("Sending message: " + message);
+}
