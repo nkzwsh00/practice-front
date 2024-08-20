092fc7ccad1ba120fcfd35bcd3ac741c62332435
nkzwsh00 <carpediem.imk@gmail.com>
2024-08-20 22:13:30 +0900

    feat: 型解決

--- study-css/src/PrimaryButton.tsx
+++ study-css/src/PrimaryButton.tsx
@@ -1,4 +1,6 @@
-const PrimaryButton = ({ children, ...props }) => {
+import { ReactNode } from "react";
+
+const PrimaryButton = ({ children, ...props }: { children: ReactNode }) => {
   return (
     <button
       className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
