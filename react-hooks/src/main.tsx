5253252bd2438542e97498f11103554b5fe2e302
nkzwsh00 <carpediem.imk@gmail.com>
2024-10-05 11:58:51 +0900

    feat: Add contact route and component

--- react-hooks/src/main.tsx
+++ react-hooks/src/main.tsx
@@ -4,12 +4,14 @@ import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./routes/root";
 import ErrorPage from "./error-page";
+import Contact from "./routes/contact";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    children: [{ path: "contacts/:contactId", element: <Contact /> }],
   },
 ]);
 
