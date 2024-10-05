04bb47517c0ecf5223f0ce093dc055405b57e5ce
nkzwsh00 <carpediem.imk@gmail.com>
2024-10-05 12:14:22 +0900

    scaffold tailwindcss

--- react-hooks/src/routes/root.tsx
+++ react-hooks/src/routes/root.tsx
@@ -4,7 +4,7 @@ export default function Root() {
   return (
     <>
       <div id="sidebar">
-        <h1>React Router Contacts</h1>
+        <h1 className="text-cyan-500">React Router Contacts</h1>
         <div>
           <form id="search-form" role="search">
             <input
