fd1cadd829cd774fe32370f71385663a3514c9cf
nkzwsh00 <carpediem.imk@gmail.com>
2024-10-07 20:49:13 +0900

    Refactor Learn component to use fragment syntax

--- react-hooks/src/Learn.tsx
+++ react-hooks/src/Learn.tsx
@@ -2,10 +2,12 @@ import { FC } from "react";
 
 export const Learn: FC = () => {
   return (
-    <div>
-      <h1 className="font-bold text-lg">Learn</h1>
-      <p>This is the learn page. You can learn things here.</p>
-    </div>
+    <>
+      <div>
+        <h1 className="font-bold text-lg">Learn</h1>
+        <p>This is the learn page. You can learn things here.</p>
+      </div>
+    </>
   );
 };
 
