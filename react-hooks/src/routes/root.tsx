18abaa09d56e717149d03788a4dd5fc5c1833c93
nkzwsh00 <carpediem.imk@gmail.com>
2024-10-14 15:26:35 +0900

    Refactor Root component to use flex layout for better UI presentation

--- react-hooks/src/routes/root.tsx
+++ react-hooks/src/routes/root.tsx
@@ -3,15 +3,33 @@ import { Link, Outlet } from "react-router-dom";
 export const Root = () => {
   return (
     <>
-      <div className="bg-slate-400 border border-cyan-200">
-        <h1>TITLE</h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to={`ajax`}>ajax</Link>
-            </li>
-          </ul>
-        </nav>
+      <div className="bg-slate-400 border border-cyan-200 flex flex-col py-1 px-2">
+        <h1 className="font-bold">TITLE</h1>
+        <div className="relative group">
+          menu
+          <div className="hidden group-hover:block absolute bg-white border border-gray-300">
+            <nav className="p-2">
+              <ul className="space-y-1">
+                <li>
+                  <Link
+                    to={`/`}
+                    className="block px-4 py-2 text-gray-700 hover:bg-gray-200"
+                  >
+                    top
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to={`ajax`}
+                    className="block px-4 py-2 text-gray-700 hover:bg-gray-200"
+                  >
+                    ajax
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
       </div>
       <div id="detail">
         <Outlet />
