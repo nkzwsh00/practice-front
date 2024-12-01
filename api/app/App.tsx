9acc509592c80f435db7e01bdc128aa29cea0273
nkzwsh00 <carpediem.imk@gmail.com>
2024-12-02 00:02:08 +0900

    GitHubユーザー情報を取得する機能を追加し、ボタンを介してユーザー情報を表示するようにしました

--- api/app/App.tsx
+++ api/app/App.tsx
@@ -1,9 +1,30 @@
 function App() {
+  function fetchUserInfo(userId: string) {
+    fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
+      .then((response) => {
+        console.log(response.status);
+        // エラーレスポンスが返されたことを検知する
+        if (!response.ok) {
+          console.error("エラーレスポンス", response);
+        } else {
+          return response.json().then((userInfo) => {
+            console.log(userInfo);
+          });
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
   return (
     <>
       <h1 className="text-3xl text-red-600 font-bold underline">
         Github User Info
       </h1>
+      <button onClick={() => fetchUserInfo("js-primer-example")}>
+        Get user info
+      </button>
     </>
   );
 }
