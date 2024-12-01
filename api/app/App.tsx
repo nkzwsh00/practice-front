79e9e7f2373ef1540739f0cbef4fd2aae869732b
nkzwsh00 <carpediem.imk@gmail.com>
2024-12-02 00:11:29 +0900

    GitHubユーザー情報を取得し、表示する機能を追加

--- api/app/App.tsx
+++ api/app/App.tsx
@@ -1,4 +1,23 @@
+import { useState } from "react";
+
+type UserInfo = {
+  name: string;
+  login: string;
+  avatar_url: string;
+  location: string;
+  public_repos: number;
+};
+
+const defaultUserInfo: UserInfo = {
+  name: "",
+  login: "",
+  avatar_url: "",
+  location: "",
+  public_repos: 0,
+};
+
 function App() {
+  const [userInfo, setUserInfo] = useState(defaultUserInfo);
   function fetchUserInfo(userId: string) {
     fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
       .then((response) => {
@@ -9,6 +28,7 @@ function App() {
         } else {
           return response.json().then((userInfo) => {
             console.log(userInfo);
+            setUserInfo(userInfo);
           });
         }
       })
@@ -22,9 +42,21 @@ function App() {
       <h1 className="text-3xl text-red-600 font-bold underline">
         Github User Info
       </h1>
-      <button onClick={() => fetchUserInfo("js-primer-example")}>
-        Get user info
-      </button>
+      <button onClick={() => fetchUserInfo("nkzwsh00")}>Get user info</button>
+      {userInfo.name !== "" && (
+        <div>
+          <h4>
+            {userInfo.name} (@{userInfo.login})
+          </h4>
+          <img src={userInfo.avatar_url} alt={userInfo.login} height="100" />
+          <dl>
+            <dt>Location</dt>
+            <dd>{userInfo.location}</dd>
+            <dt>Repositories</dt>
+            <dd>{userInfo.public_repos}</dd>
+          </dl>
+        </div>
+      )}
     </>
   );
 }
